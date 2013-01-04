3e2b7fad75ec5328871d3119dd8cec5864d71fed
Beben <beben@beben-laptop.numericable.fr>
2013-01-04 17:38:12 +0100

    Algo client / server + cmd prompt

---
+++ src/client.c
@@ -0,0 +1,49 @@
+/*
+ * client.c
+ * 
+ * Copyright 2013 Benjamin Zigh <benj@minzigh.com>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ * 
+ * 
+ */
+ 
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include "stream_manager.h"
+#include "project.h" //verifier les includes
+
+
+
+int main(int argc, char ***********************************argv) {
+	request_t *req = NULL;
+	char ans_buf[BUFSIZ];
+	//sigaction
+	char cmd_buf[BUFSIZ];
+	
+	int res= 0
+	while (res != -233) //code exit
+		//scanf	
+		//res = get request(req)
+		//create
+		//open 
+		//send
+		//delete request
+	//close
+	//unlink
+	return EXIT_SUCCESS;
+}
--- src/server.c
+++ src/server.c
@@ -2,53 +2,68 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include "stream_manager.h"
-#include "project.h"
+#include "project.h" //verifier les includes
 
-//tester les appels de stream_* ?
+//tester les appels de stream_*?  oui!!
 
 void help(){
-	printf("./server uid | gid | pid\n"); // a changer
+	printf("./server \n"); // a changer
 }
 
 void quit(){
 	//peut etre factoriser ici l'envoie de signaux
 }
 
-void process_request(request_t req) {
+void process_request(request_t *req) {
     char ans_name[STRSIZ];
     sprintf(ans_name, "%s%i", answer_prefix, req.clientpid);
-    stream_t req_str = manager_getstream(argv[argc-1]);
+    stream_t req_str = manager_getstream();//a completer avec le type de la req
     stream_open(&req_str, ans_name, )
+    //envoyer
+    //fermer
 
 }
 
 int main(int argc, char **argv) {
 
-	if (argc != 2){
+	if (argc != 2){ // ou pas
 		help();
 		exit(EXIT_FAILURE);
 	}
-
-	stream_t req_str = manager_getstream(argv[argc-1]);
+	//sigaction
+	stream_t req_str = manager_getstream(); //a completer
+	//3 getstream (tableau) en dur pour l'instant
+	//un set avec les 3
 	int n = 0;
 	request_t req;
 
+	//creer map (clientpid, tube)
+
+	//stream create (+ stream open!)
 
 
-	//stream create (+ stream open?)
-
-
-	while ((n = stream_read(&req_str, &req, sizeof(request_t))) > 0) {
+	while ((n = stream_read(&req_str, &req, sizeof(request_t))) > 0) { //sizeof -> buffersize  read -> select > 0 || errno = INTR
+	//for tous les types de stream (isset?)
+	//si client pid pas dans map
+		//creer tube
+		//ajouter pid,tube a la map
 		switch(fork()) {
 			case -1:
 				//envoyer message (signal)
-				check_error2(1, "fork"); // faire une constante
+				// faire une constante?
 			case 0:
-				process_request(req);
-				exit(EXIT_SUCCESS);
-            //reprendre algo td
-
-
-
-
+				//while read tube
+				process_request(&req);
+				//close tube
+				exit(EXIT_SUCCESS); // -> raise
+				//reprendre algo td
+			default:
+				//ecrire tube
+				//close tube
+				//retour
+		}
+	}
+	//close
+	//unlink
+	
 }
