3729063c7d9e01416ed7335b95b3d681ec6a2d5f
Beben <beben@beben-laptop.numericable.fr>
2013-01-04 15:59:39 +0100

    Server.c trop bien trop bien!

--- src/server.c
+++ src/server.c
@@ -1,45 +1,54 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include "const.h"
-#include "project.h" //c'est la même chose bordel!!
+#include "stream_manager.h"
+#include "project.h"
+
+//tester les appels de stream_* ?
 
 void help(){
-	printf("Ca marche pas comme ça ducon\n"); // a changer
+	printf("./server uid | gid | pid\n"); // a changer
 }
 
 void quit(){
-	//
-	//
+	//peut etre factoriser ici l'envoie de signaux
 }
 
+void process_request(request_t req) {
+    char ans_name[STRSIZ];
+    sprintf(ans_name, "%s%i", answer_prefix, req.clientpid);
+    stream_t req_str = manager_getstream(argv[argc-1]);
+    stream_open(&req_str, ans_name, )
+
+}
 
 int main(int argc, char **argv) {
-	
+
 	if (argc != 2){
 		help();
 		exit(EXIT_FAILURE);
 	}
-	
-	stream_t s = getStream(argv[argc-1]);
+
+	stream_t req_str = manager_getstream(argv[argc-1]);
 	int n = 0;
 	request_t req;
-	
-	stream_open(&s, request_name, O_CREAT | O_WRONLY | O_TRUNC
-				, S_IRUSR | S_IWUSR | S_IWGRP | S_IWOTH
-				, sizeof(request_t));
-	
-	while ((n = stream_read(&s, &req, sizeof(request_t))) > 0) {
+
+
+
+	//stream create (+ stream open?)
+
+
+	while ((n = stream_read(&req_str, &req, sizeof(request_t))) > 0) {
 		switch(fork()) {
 			case -1:
-				//envoyer message (signal custom)
+				//envoyer message (signal)
 				check_error2(1, "fork"); // faire une constante
 			case 0:
-				process_request();
+				process_request(req);
 				exit(EXIT_SUCCESS);
-				
-	
-	
-	
-		
+            //reprendre algo td
+
+
+
+
 }
